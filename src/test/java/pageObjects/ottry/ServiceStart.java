41b7020aa7a40558cad4be10ed6fa7625891bc9c
georgii.lebeda <georgii.lebeda@resource.corp>
2017-10-20 11:11:05 +0300

    Add Service start interface

--- src/test/java/pageObjects/ottry/ServiceStart.java
+++ src/test/java/pageObjects/ottry/ServiceStart.java
@@ -1,4 +1,12 @@
 package pageObjects.ottry;
 
-public interface ServiceMain {
+import utils.core.BasePage;
+
+public interface ServiceStart {
+
+    //method for navigating to needed page
+    void navigateToPage(String URL);
+
+    BaseServicePage goBtnClick();
+
 }
